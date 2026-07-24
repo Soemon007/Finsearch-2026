8247bb5231112735b456670647c913df178533ed
Soemon007 <rehanmallik31@gmail.com>
2026-07-24 13:12:17 +0530

    added test train split

--- feature_engineering.py
+++ feature_engineering.py
@@ -4,12 +4,21 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
 
-
+# Transforming Data
 OHLCV_data['Returns'] = OHLCV_data["Open"].pct_change()
 OHLCV_data['LogReturns'] = np.log(1 + OHLCV_data['Returns'])
 
 series = OHLCV_data['LogReturns'].dropna()
 prices = OHLCV_data['Open'].loc[series.index]
+
+# Test - Train split
+split =  int(0.8 * len(series))
+
+train_series = series[:split]
+test_series = series[split:]
+
+train_prices = series[:split]
+test_prices= series[split:]
 
 result = adfuller(series)
 
