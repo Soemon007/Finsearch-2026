cd3e4fc58594cbcf9d57c4db210bf25147599771
Soemon007 <rehanmallik31@gmail.com>
2026-07-25 20:24:04 +0530

    Made feature engineering modular

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,26 +1,21 @@
-from preprocessing import OHLCV_data
 import numpy as np
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-import matplotlib.pyplot as plt
-
-# Transforming Data
-OHLCV_data['Returns'] = OHLCV_data["Open"].pct_change()
-OHLCV_data['LogReturns'] = np.log(1 + OHLCV_data['Returns'])
-
-series = OHLCV_data['LogReturns'].dropna()
-prices = OHLCV_data['Open'].loc[series.index]
-
-# Test - Train split
-split =  int(0.8 * len(series))
-
-train_series = series[:split]
-test_series = series[split:]
-
-train_prices = prices[:split]
-test_prices= prices[split:]
-
-result = adfuller(series)
 
 
+def feature_engineering(data):
+    # Transforming Data
+    data['Returns'] = data["Open"].pct_change()
+    data['LogReturns'] = np.log(1 + data['Returns'])
 
+    series = data['LogReturns'].dropna()
+    prices = data['Open'].loc[series.index]
+
+    # Test - Train split
+    split =  int(0.8 * len(series))
+
+    train_series = series[:split]
+    test_series = series[split:]
+
+    train_prices = prices[:split]
+    test_prices= prices[split:]
+
+    return train_series, test_series, train_prices, test_prices
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from preprocessing import preprocessing
+from feature_engineering import feature_engineering
 
 def main():
     # Loading the Data
     OHLCV_data = "Historical_Data.csv"
 
-    #Preprocessing Data
-    preprocessing(OHLCV_data)
+    # Preprocessing Data
+    df = preprocessing(OHLCV_data)
     
+    # Feature Engineering
+    train_series, test_series, train_prices, test_prices = feature_engineering(df)
 
 
 if __name__ == "__main__":
