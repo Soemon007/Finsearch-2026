cbec820ca916223f503248cf1a82402986504111
Soemon007 <rehanmallik31@gmail.com>
2026-07-25 20:43:10 +0530

    Made ARIMA modular

--- ARIMA.py
+++ ARIMA.py
@@ -1,54 +1,57 @@
-from feature_engineering import train_series, test_series, train_prices, test_prices
 from statsmodels.tsa.arima.model import ARIMA
 from backtest import backtester
-from AIC import best_order
 import matplotlib.pyplot as plt
 import numpy as np
 
-# MODEL FITTING
-model = ARIMA(train_series, order = best_order)
-results = model.fit()
+def ARIMA_modelling(best_order, train_series):
 
-print(results.summary())
+    # MODEL FITTING
+    model = ARIMA(train_series, order = best_order)
+    results = model.fit()
 
-# FORECASTING
+    print(results.summary())
+    return results
 
-test_results = results.apply(test_series)
-forecast = test_results.fittedvalues
+def Forecast(results, test_series, test_prices):
 
-# TRADING:
-# One-way cost = 0.01% fee + 0.02% slippage = 0.03% (0.0003)
-# Round-trip cost = 0.06% (0.0006). Demand at least 0.08% return to enter a trade.
-min_expected_return = 0.0008
+    # FORECASTING
+    test_results = results.apply(test_series)
+    forecast = test_results.fittedvalues
 
-signals = []
-for val in forecast:
-    if val > min_expected_return:
-        signals.append(1)    # Strong Buy: expected return beats fees
-    elif val < -min_expected_return:
-        signals.append(-1)   # Strong Sell: exit position
-    else:
-        signals.append(0)    # Hold: forecast is too weak to justify trading costs
+    # TRADING:
+    min_expected_return = 0.0008
 
-# SIMULATION
+    signals = []
+    for val in forecast:
+        if val > min_expected_return:
+            signals.append(1)   
+        elif val < -min_expected_return:
+            signals.append(-1)  
+        else:
+            signals.append(0) 
+    # SIMULATION
 
-arima_portfolio = backtester(
-    signals,
-    test_prices
-)
+    arima_portfolio = backtester(
+        signals,
+        test_prices
+    )
 
-print(arima_portfolio.head())
-print(forecast.head())
+    return arima_portfolio, forecast, signals
 
-values, counts = np.unique(signals, return_counts=True)
+def ARIMA_summary(arima_portfolio, forecast, signals):
+    
+    print(arima_portfolio.head())
+    print(forecast.head())
 
-for v, c in zip(values, counts):
-    print(v, c)
+    values, counts = np.unique(signals, return_counts=True)
 
-plt.figure(figsize=(12,6))
-plt.plot(arima_portfolio["PortfolioValue"])
-plt.title("ARIMA Portfolio Value")
-plt.xlabel("Trading Day")
-plt.ylabel("Portfolio Value")
-plt.grid(True)
-plt.show()
+    for v, c in zip(values, counts):
+        print(v, c)
+
+    plt.figure(figsize=(12,6))
+    plt.plot(arima_portfolio["PortfolioValue"])
+    plt.title("ARIMA Portfolio Value")
+    plt.xlabel("Trading Day")
+    plt.ylabel("Portfolio Value")
+    plt.grid(True)
+    plt.show()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from preprocessing import preprocessing
 from feature_engineering import feature_engineering
 from AIC import AIC
+from ARIMA import ARIMA_modelling, Forecast, ARIMA_summary
 
 def main():
     # Loading the Data
@@ -15,6 +16,11 @@ def main():
     # AIC testing
     best_order = AIC(train_series)
 
+    #ARIMA
+    results = ARIMA_modelling(best_order, train_series)
+    arima_portfolio, forecast, signals = Forecast(results, test_series, test_prices)
+
+    ARIMA_summary(arima_portfolio, forecast, signals)
 
 if __name__ == "__main__":
     main()
