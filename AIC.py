121e6e60a8bed868598d98ed8020fec5c08b5d6a
Soemon007 <rehanmallik31@gmail.com>
2026-07-25 20:28:20 +0530

    Made AIC modular

--- AIC.py
+++ AIC.py
@@ -1,32 +1,32 @@
-import warnings
 from statsmodels.tsa.arima.model import ARIMA
+
+import warnings
 from statsmodels.tools.sm_exceptions import ConvergenceWarning
-from feature_engineering import train_series
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
-best_aic = float("inf")
-best_order = None
-best_model = None
+def AIC(train_series):
+    best_aic = float("inf")
+    best_order = None
+    best_model = None
 
-for p in range(4):
-    for q in range(4):
+    for p in range(4):
+        for q in range(4):
 
-        try:
-            model = ARIMA(train_series, order=(p, 0, q))
-            result = model.fit()
+            try:
+                model = ARIMA(train_series, order=(p, 0, q))
+                result = model.fit()
 
-            print(f"ARIMA({p},0,{q}) -> AIC = {result.aic:.2f}")
+                print(f"ARIMA({p},0,{q}) -> AIC = {result.aic:.2f}")
 
-            if result.aic < best_aic:
-                best_aic = result.aic
-                best_order = (p, 0, q)
-                best_model = result
+                if result.aic < best_aic:
+                    best_aic = result.aic
+                    best_order = (p, 0, q)
+                    best_model = result
 
-        except Exception as e:
-            print(f"ARIMA({p},0,{q}) failed: {e}")
+            except Exception as e:
+                print(f"ARIMA({p},0,{q}) failed: {e}")
 
-print("Best Order :", best_order)
-print("Best AIC   :", best_aic)
+    print("Best Order :", best_order)
+    print("Best AIC   :", best_aic)
 
-if best_model is not None:
-    print(best_model.summary())
+    return best_order, best_aic
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from preprocessing import preprocessing
 from feature_engineering import feature_engineering
+from AIC import AIC
 
 def main():
     # Loading the Data
@@ -10,6 +11,9 @@ def main():
     
     # Feature Engineering
     train_series, test_series, train_prices, test_prices = feature_engineering(df)
+
+    # AIC testing
+    best_order = AIC(train_series)
 
 
 if __name__ == "__main__":
